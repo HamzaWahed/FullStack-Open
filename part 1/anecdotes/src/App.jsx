569b2be495cf2a7942ce535a7a8b8c7020f6f443
unknown <hamzawahed@dal.ca>
2023-09-21 11:07:13 -0300

    anecdotes

---
+++ part 1/anecdotes/src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react'
+
+const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+
+  const nextAnecdote = () => {
+    const select = Math.floor(Math.random() * anecdotes.length)
+    setSelected(select)
+  }
+
+  const updateVote = (index) => {
+    const copy = [...votes]
+    copy[index] = copy[index] + 1
+    setVotes(copy)
+  }
+
+  const getAnecdoteWithMostVotes = () => {
+    let maxIndex = 0
+    for (let i=1; i<votes.length; i++) {
+      if (votes[i] > votes[maxIndex]) {
+        maxIndex = i
+      }
+    }
+    return maxIndex
+  }
+
+  const index = getAnecdoteWithMostVotes()
+
+  return (
+    <div>
+      <div>
+        <h1>Anecdote of the day</h1>
+        {anecdotes[selected]}
+        <br />
+        Has votes {votes[selected]}
+        <br />
+        <Button handleClick={() => updateVote(selected)} text='vote' />
+        <Button handleClick={nextAnecdote} text='next anecdote'/>
+      </div>
+      <div>
+        <h1>Anecdote with most votes</h1>
+          {anecdotes[index]}
+          <br />
+          has votes {votes[index]}
+      </div>
+    </div>
+  )
+}
+
+export default App
