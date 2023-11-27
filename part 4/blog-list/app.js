92c82188953e5135d6f3ca72fa7dc68336ab2b46
CB <cb@CBs-MacBook-Pro.local>
2023-11-27 12:07:43 -0400

    4.2

---
+++ part 4/blog-list/app.js
@@ -0,0 +1,26 @@
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const blogRouter = require("./controllers/blogs");
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+
+console.log("connection to", config.MONGO_URI);
+mongoose
+  .connect(config.MONGO_URI)
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB", error.message);
+  });
+
+app.use(cors());
+app.use(express.static("dist"));
+app.use(express.json());
+
+app.use("/api/blogs", blogRouter);
+
+module.exports = app;
